7b31271052f1201cd13ed939576dc9b66e8f76aa
Jhonatan Alejandro Mejia Mendoza <jhobatrix@gmail.com>
2023-06-07 12:17:36 -0500

    initial commit

---
+++ models/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const { socketController } = require("../sockets/controller");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    this.server = require("http").createServer(this.app);
+    this.io = require("socket.io")(this.server);
+
+    this.paths = {};
+
+    //Middlewares
+    this.middlewares();
+
+    //Rutas de mi aplicacion
+    this.routers();
+
+    //Sockets
+    this.sockets();
+  }
+
+  middlewares() {
+    //CORS
+    this.app.use(cors());
+
+    //directorio publico
+    this.app.use(express.static("public"));
+  }
+  routers() {
+    // this.app.use(this.paths.auth, require("../routes/auth"));
+  }
+
+  sockets() {
+    this.io.on("connection", socketController);
+  }
+
+  listen() {
+    this.server.listen(this.port, () => {
+      console.log(`Servidor corriendo por el puerto ${this.port}`);
+    });
+  }
+}
+module.exports = Server;
